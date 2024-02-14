96cd0fdb16f55db6a2e8a18dbd57325d5d647cda
Roman <dmot.boss@mail.ru>
2024-02-14 20:43:31 +0300

    add lesson3: task2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintSquares(int n)
+{
+    int i = 1;
+    while(i <= n)
+    {
+        Console.Write(i * i + " ");
+        i++;
+    }
+    Console.WriteLine();
+}
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
