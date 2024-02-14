b7489d652545a2e219fd3df14956a61918568388
Roman <dmot.boss@mail.ru>
2024-02-14 20:07:46 +0300

    add lesson3: task1

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    int nomenator = a * b;
+    double denomenator = c + d;
+    double result = nomenator / denomenator;
+    return result;
+}
+
+Console.WriteLine(CalculateFormula(1, 2, 3, 4));
